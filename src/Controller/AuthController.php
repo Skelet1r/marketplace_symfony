e244613f99ce80f0a7dd67e44cc8e5ec2d7fe1ce
Skelet1r <oleksiidmytrevych@gmail.com>
2024-12-08 08:48:31 +0200

    Готовая система пользователя, (авторизация, аутентификация, логаут)

--- src/Controller/AuthController.php
+++ src/Controller/AuthController.php
@@ -12,9 +12,19 @@ use Symfony\Component\HttpFoundation\Request;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Validator\Constraints\Regex;
+use Symfony\Component\Security\Http\Attribute\CurrentUser;
 
 class AuthController extends AbstractController
 {
+    private $passwordHasher;
+    private $entityManager;
+
+    public function __construct(UserPasswordHasherInterface $passwordHasher, EntityManagerInterface $entityManager)
+    {
+        $this->passwordHasher = $passwordHasher;
+        $this->entityManager = $entityManager;
+    }
+
     #[Route('/auth', name: 'app_auth')]
     public function index(): Response
     {
@@ -46,4 +56,38 @@ class AuthController extends AbstractController
         return $this->json(['Form is not valid', $form]);
     }
 
+    #[Route('/login', name: 'login')]
+    public function auth(#[CurrentUser] ?User $user, Request $request): JsonResponse{
+        if (null === $user) {
+            return $this->json([
+                'message' => 'missing credentials',
+            ], Response::HTTP_UNAUTHORIZED);
+        }
+            
+         // somehow create an API token for $user
+        $token = 'sigma';   
+
+        return $this->json([
+            'user'  => $user->getUserIdentifier(),                 
+            'token' => $token
+        ]);
+    }
+
+    //проверка на аутентификацию
+    #[Route('/check-auth', name: 'check_auth')]
+    public function checkAuth(#[CurrentUser] ?User $user): JsonResponse
+    {
+        return $this->json([
+            'authenticated' => $user !== null,
+            'user' => $user ? $user->getUserIdentifier() : null,
+        ]);
+    }
+
+
+    #[Route('/logout', name:'logout')]
+    public function logout(): JsonResponse{
+        return $this->json([
+            'message' => 'Successfully logout!'
+        ]);
+    }
 }
